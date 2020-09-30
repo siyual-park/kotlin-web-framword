278faf2827bb864255554779d631f5d3e805975c
siyual-park <siyual.bak@gmail.com>
2020-09-30 12:25:58 +0900

    단순화

--- src/main/kotlin/com/ara/container/ConcurrentContainer.kt
+++ src/main/kotlin/com/ara/container/ConcurrentContainer.kt
@@ -1,15 +1,12 @@
 package com.ara.container
 
-import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KClass
-import kotlin.reflect.full.superclasses
 
 @Suppress("UNCHECKED_CAST")
 class ConcurrentContainer : Container {
     private val values: MutableMap<KClass<*>, Any> = ConcurrentHashMap()
     private val providers: MutableMap<KClass<*>, Provider<*>> = ConcurrentHashMap()
-    private val classRelations: MutableMap<KClass<*>, MutableSet<ClassRelation<*>>> = ConcurrentHashMap()
 
     override val size: Int
         get() = values.size
@@ -18,91 +15,45 @@ class ConcurrentContainer : Container {
         get() = providers.size
 
     override fun <T : Any> unregisterAll(clazz: KClass<T>): Container {
-        values.remove(clazz)
-        providers.remove(clazz)
-        unregisterClassRelations(clazz)
+        unregister(clazz)
+        unregisterProvider(clazz)
         return this
     }
 
     override fun <T : Any> unregister(clazz: KClass<T>): Container {
         values.remove(clazz)
-        unregisterClassRelations(clazz)
         return this
     }
 
     override fun <T : Any> unregisterProvider(clazz: KClass<T>): Container {
         providers.remove(clazz)
-        unregisterClassRelations(clazz)
         return this
     }
 
-    override fun <T : Any, V : T> register(clazz: KClass<T>, value: V): Container {
+    override fun <T : Any> register(clazz: KClass<T>, value: T): Container {
         values[clazz] = value
-        registerClassRelations(clazz)
         return this
     }
 
-    override fun <T : Any, V : T> registerProvider(clazz: KClass<T>, provider: Container.() -> V): Container {
+    override fun <T : Any> registerProvider(clazz: KClass<T>, provider: Container.() -> T): Container {
         val container = this
-        return registerProvider(clazz, object : Provider<V> {
-            override fun get(): V {
+        return registerProvider(clazz, object : Provider<T> {
+            override fun get(): T {
                 return provider(container)
             }
         })
     }
 
-    override fun <T : Any, V : T> registerProvider(clazz: KClass<T>, provider: Provider<V>): Container {
+    override fun <T : Any> registerProvider(clazz: KClass<T>, provider: Provider<in T>): Container {
         providers[clazz] = provider
-        registerClassRelations(clazz)
         return this
     }
-
-    private fun <T : Any> unregisterClassRelations(clazz: KClass<T>) {
-        classRelations.forEach { (_, classRelations) ->
-            classRelations.removeIf { it.clazz === clazz }
-        }
-    }
-
-    private fun <T : Any> registerClassRelations(clazz: KClass<T>) {
-        getOrCreateClassRelations(clazz)
-        clazz.superclasses.forEachIndexed { i, superClass ->
-            getOrCreateClassRelations(superClass)
-                .add(ClassRelation(clazz, i + 1))
-        }
-    }
-
-    private fun <T : Any> getOrCreateClassRelations(clazz: KClass<T>): MutableSet<ClassRelation<*>> {
-        val set = classRelations.getOrPut(clazz) {
-            Collections.synchronizedSortedSet(TreeSet { a, b ->
-                a.diff - b.diff
-            })
-        }
-        set.add(ClassRelation(clazz, 0))
-        return set
-    }
-
 
     override fun <T : Any> resolve(clazz: KClass<T>): T {
         return resolveOrNull(clazz) ?: throw RuntimeException("Cant resolve $clazz")
     }
 
     override fun <T : Any> resolveOrNull(clazz: KClass<T>): T? {
-        return classRelations[clazz]?.let { classRelations ->
-            for ((subclass) in classRelations) {
-                val value = exactlyResolveOrNull(subclass)
-                if (value != null) {
-                    return@let value
-                }
-            }
-            return@let null
-        } as T?
-    }
-
-    override fun <T : Any> exactlyResolve(clazz: KClass<T>): T {
-        return exactlyResolveOrNull(clazz) ?: throw RuntimeException("Cant resolve $clazz")
-    }
-
-    override fun <T : Any> exactlyResolveOrNull(clazz: KClass<T>): T? {
         val value = values[clazz]
         if (value != null) {
             return value as T
--- src/main/kotlin/com/ara/container/Container.kt
+++ src/main/kotlin/com/ara/container/Container.kt
@@ -12,39 +12,32 @@ interface Container {
 
     fun <T: Any> unregisterProvider(clazz: KClass<T>): Container
 
-    fun <T: Any, V: T> register(clazz: KClass<T>, value: V): Container
+    fun <T : Any> register(clazz: KClass<T>, value: T): Container
 
-    fun <T: Any, V: T> registerProvider(clazz: KClass<T>, provider: Container.() -> V): Container
+    fun <T : Any> registerProvider(clazz: KClass<T>, provider: Container.() -> T): Container
 
-    fun <T: Any, V: T> registerProvider(clazz: KClass<T>, provider: Provider<V>): Container
+    fun <T : Any> registerProvider(clazz: KClass<T>, provider: Provider<in T>): Container
 
     fun <T: Any> resolve(clazz: KClass<T>): T
 
     fun <T: Any> resolveOrNull(clazz: KClass<T>): T?
-
-    fun <T: Any> exactlyResolve(clazz: KClass<T>): T
-
-    fun <T: Any> exactlyResolveOrNull(clazz: KClass<T>): T?
 }
 
-inline fun <reified T: Any, V: T> Container.register(value: V): Container {
+inline fun <reified T : Any> Container.register(value: T): Container {
     return this.register(T::class, value)
 }
-inline fun <reified T: Any, V: T> Container.registerProvider(noinline provider: Container.() -> V): Container {
+
+inline fun <reified T : Any> Container.registerProvider(noinline provider: Container.() -> T): Container {
     return this.registerProvider(T::class, provider)
 }
-inline fun <reified T: Any, V: T> Container.registerProvider(provider: Provider<V>): Container {
+
+inline fun <reified T : Any> Container.registerProvider(provider: Provider<in T>): Container {
     return this.registerProvider(T::class, provider)
 }
-inline fun <reified T: Any> Container.resolve(): T {
+
+inline fun <reified T : Any> Container.resolve(): T {
     return this.resolve(T::class)
 }
 inline fun <reified T: Any> Container.resolveOrNull(): T? {
     return this.resolveOrNull(T::class)
-}
-inline fun <reified T: Any> Container.exactlyResolve(): T {
-    return this.exactlyResolve(T::class)
-}
-inline fun <reified T: Any> Container.exactlyResolveOrNull(): T? {
-    return this.exactlyResolveOrNull(T::class)
 }
