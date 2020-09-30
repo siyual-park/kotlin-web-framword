f52f14496b48570a86a324198faebeafafa60fe2
siyual-park <siyual.bak@gmail.com>
2020-09-30 12:36:07 +0900

    단순화

--- src/main/kotlin/com/ara/application/Application.kt
+++ src/main/kotlin/com/ara/application/Application.kt
@@ -6,9 +6,9 @@ import com.ara.container.resolve
 import com.ara.module.Module
 import com.ara.runner.Runner
 
-class Application<IN: Any, OUT: Any>(
+class Application<IN : Any, OUT : Any>(
     private val container: Container = ConcurrentContainer.create()
-) : Runner<IN, OUT> {
+) : Runner<IN, OUT>, Container by container {
     fun use(module: (Container) -> Unit): Application<IN, OUT> {
         return use(object : Module {
             override fun configure(container: Container) {
@@ -18,12 +18,12 @@ class Application<IN: Any, OUT: Any>(
     }
 
     fun use(module: Module): Application<IN, OUT> {
-        module.configure(container)
+        module.configure(this)
         return this
     }
 
     override fun run(input: IN): OUT {
-        val runner: Runner<IN, OUT> = container.resolve()
+        val runner: Runner<IN, OUT> = resolve()
         return runner.run(input)
     }
 }
